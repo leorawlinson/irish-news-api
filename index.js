3ce1caddc63afc00a93b22054d7bb88c8c08ce2a
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2022-06-27 10:28:21 +0200

    cheerio finding html elements and json displayed

--- index.js
+++ index.js
@@ -6,4 +6,30 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+const articles = [];
+
+app.get("/", (req, res) => {
+  res.json("Welcome to Irish news");
+});
+
+app.get("/news", (req, res) => {
+  axios
+    .get(`https://www.independent.ie/news/`)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $(`a:contains("news")`, html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr(`href`);
+        articles.push({
+          title,
+          url,
+        });
+      });
+      res.json(articles);
+    })
+    .catch((err) => console.log(err));
+});
+
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
